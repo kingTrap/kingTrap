c0ceeb56ef11712dc7553b66304d408f094270f0
kingtrap <kingtrap2@outlook.com>
2019-07-14 00:51:47 +0800

    【SpringBoot系列之一】：入门demo实例！

---
+++ hello/src/main/java/com/kingtrap/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.kingtrap.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+        return "Hello !";
+    }
+}
